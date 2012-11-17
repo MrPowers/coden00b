d1c8722fb05feb6cfa9d1c50874b385865bd38af
Matthew Powers <matthewkevinpowers@gmail.com>
2012-11-16 17:06:40 -0800

    added code to make pagination post numbering work on all pages.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,4 +8,12 @@ module PostsHelper
 	def uri_short(url)
 		URI(url).host.gsub("www.", "")
 	end
+
+	def post_number(counter)
+		if params[:page]
+			(params[:page].to_i - 1) * 30 + counter
+		else
+			1 * counter
+		end
+	end
 end
